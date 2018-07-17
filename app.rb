7009a6b7b54987091786bef29e1e13f760daacd8
kello22e <kello22e@mtholyoke.edu>
2018-07-17 21:05:25 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    "Hello World"
+  end
+  
+  get '/name' do
+    "My name is Emily"
+  end
+  get '/hometown' do
+    "My hometown is Olympia"
+  end
+  get '/favorite-song' do
+    "My favorite song is Redbone"
+  end
 end
